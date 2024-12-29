a366db943c5110c9324343508c2577f823df222e
Juhan Ahmed <azurarobo280@gmail.com>
2024-12-30 05:03:54 +0600

    Initial

---
+++ app.ts
@@ -0,0 +1,80 @@
+import "./env.d.ts"
+import { App } from "astal/gtk3";
+import style from "./style.scss";
+import WhichKey, { BindProps } from "./widget/WhichKey";
+import { exec, Variable } from "astal";
+
+const visible = Variable(false);
+const binds: Variable<BindProps[][]> = Variable([]);
+
+App.start({
+	instanceName: "hyprwhichkey",
+	css: style,
+	main() {
+		let wk = WhichKey({ binds });
+		visible.subscribe(vis => wk.set_visible(vis));
+	},
+	requestHandler(submap, res) {
+		console.log(submap);
+		if (!submap) {
+			visible.set(false);
+			return res("ok");
+		}
+
+		if (submap === "base") {
+			if (visible.get()) {
+				visible.set(false);
+				return res("ok");
+			}
+			setBinds("", 3);
+		} else {
+			submap = JSON.parse(submap).submap;
+			if (!submap) {
+				visible.set(false);
+				return res("ok");
+			}
+
+			setBinds(submap, 4);
+		}
+
+		visible.set(true);
+		res("ok");
+	},
+})
+
+const extra = [
+	{
+		key: "[1-9,0]",
+		modmask: 64,
+		submap: "",
+		has_description: true,
+		description: "Switch to workspace [1-10]"
+	},
+	{
+		key: "[1-9,0]",
+		modmask: 65,
+		submap: "",
+		has_description: true,
+		description: "Move to workspace [1-10]"
+	},
+] as BindProps[];
+
+let last_rows = 0;
+function setBinds(submap: string, columns: number) {
+	let b: BindProps[] = JSON.parse(exec(["hyprctl", "binds", "-j"]));
+	b.unshift(...extra);
+	b = b.filter(bind => bind.submap === submap && bind.arg !== "reset" && bind.has_description)
+		.sort(bind => +(bind.dispatcher === "submap") - 0.5);
+	const rows = Math.max(Math.ceil(b.length / columns), 4);
+
+	let r: BindProps[][] = [];
+	for (let i = 0; i < columns; i++)
+		r.push(b.splice(0, rows));
+	binds.set(r);
+
+	// when it transitions from having more rows to fewer
+	// you could see the un-rendered part of the widget
+	// this skips the transition phase by hiding it
+	if(last_rows > rows) visible.set(false);
+	last_rows = rows;
+}
---
+++ widget/WhichKey.tsx
@@ -0,0 +1,74 @@
+import { App, Astal } from "astal/gtk3"
+import {  Variable } from 'astal'
+import Grid from "../Grid";
+
+declare const React: any;
+
+export interface BindProps {
+	locked: boolean;
+	mouse: boolean;
+	release: boolean;
+	repeat: boolean;
+	longPress: boolean;
+	non_consuming: boolean;
+	has_description: boolean;
+	modmask: number;
+	submap: string;
+	key: string;
+	keycode: number;
+	catch_all: boolean;
+	description: string;
+	dispatcher: string;
+	arg: string;
+}
+
+const modkeys = ["shift", , , "alt", , , "mod"];
+function modmaskToKeys(modmask: number): string {
+	return modkeys
+		.filter((_, i) => modmask >> i & 1)
+		.map(key => `<${key}>`)
+		.reverse()
+		.join(" ")
+}
+
+function key(entry: BindProps, padding = 0) {
+	return `${modmaskToKeys(entry.modmask)} ${entry.key}`.padStart(padding)
+}
+
+function Keybind({ entry, padding }: { entry: BindProps, padding: number }) {
+	return <box className="keybind">
+		<label className="key">{key(entry, padding)}</label>
+		<label className="arrow">  </label>
+		<label className={entry.dispatcher === "submap" ? "layout-submap" : "layout-desc"}>
+			{entry.description}
+		</label>
+	</box>
+}
+
+export default function WhichKey({ binds }: { binds: Variable<BindProps[][]> }) {
+	const { BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
+
+	return <window
+		name="WhichKey"
+		className="whichkey"
+		namespace="hyprland_which_key"
+		layer={Astal.Layer.OVERLAY}
+		visible={false}
+		monitor={0}
+		anchor={BOTTOM | LEFT | RIGHT}
+		application={App}>
+		{binds(binds => <Grid
+			column-homogeneous
+			rowSpacing={2}
+			setup={self => {
+				for (let i = 0; i < binds.length; i++) {
+					let padding = 0;
+					for (let j = 0; j < binds[i].length; j++)
+						if (padding < key(binds[i][j]).length) padding = key(binds[i][j]).length;
+
+					for (let j = 0; j < binds[i].length; j++)
+						self.attach(<Keybind entry={binds[i][j]} padding={padding} />, i, j, 1, 1);
+				}
+			}} />)}
+	</window>
+}
